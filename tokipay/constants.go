acaab344f5291ec8e4f8cde3b1d044a60c410cd7
Cro8ox <croak.g@gmail.com>
2023-09-02 16:49:29 +0800

    fix

--- tokipay/constants.go
+++ tokipay/constants.go
@@ -1,9 +1,9 @@
 package tokipay
 
 const (
-	OrderStatusPaid     string = "APPROVED"
-	orderStatusCompletd string = "COMPLETED"
-	OrderStatusPending  string = "PENDING"
-	OrderStatusCanceled string = "EXPIRED"
-	OrderStatusExpired  string = "EXPIRED"
+	OrderStatusPaid      string = "APPROVED"
+	orderStatusCompleted string = "COMPLETED"
+	OrderStatusPending   string = "PENDING"
+	OrderStatusCanceled  string = "EXPIRED"
+	OrderStatusExpired   string = "EXPIRED"
 )
