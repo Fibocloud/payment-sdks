24518a700de8eb93cd211777db16041d6ae1913f
Davaajantsan Batorgil <batorgil.it@gmail.com>
2024-02-07 14:43:37 +0800

    f

--- tokipay_thirdparty/apis.go
+++ tokipay_thirdparty/apis.go
@@ -12,39 +12,11 @@ import (
 
 // SocialPay
 var (
-	TokipayPaymentQr = utils.API{
-		Url:    "/jump/v4/spose/payment/request",
-		Method: http.MethodPost,
-	}
 	TokipayPaymentSentUser = utils.API{
-		Url:    "/jump/v4/spose/payment/user-request",
-		Method: http.MethodPost,
-	}
-	TokipayPaymentScanUser = utils.API{
-		Url:    "/jump/v4/spose/payment/scan/user-request",
-		Method: http.MethodPost,
-	}
-	TokipayPaymentStatus = utils.API{
-		Url:    "/jump/v4/spose/payment/status?requestId=",
-		Method: http.MethodGet,
-	}
-	TokipayPaymentCancel = utils.API{
-		Url:    "/jump/v4/spose/payment/request?requestId=",
-		Method: http.MethodDelete,
-	}
-	TokipayRefund = utils.API{
-		Url:    "/jump/v4/spose/payment/refund",
-		Method: http.MethodPut,
-	}
-	TokipayDeeplink = utils.API{
-		Url:    "/jump/v1/third-party/payment/deeplink",
-		Method: http.MethodPost,
-	}
-	TokipayPhoneRequest = utils.API{
 		Url:    "/jump/v1/third-party/payment/request",
 		Method: http.MethodPost,
 	}
-	TokipayTransactionStatus = utils.API{
+	TokipayPaymentStatus = utils.API{
 		Url:    "/jump/v1/third-party/payment/status?requestId=",
 		Method: http.MethodGet,
 	}
@@ -63,30 +35,6 @@ func (q *tokipayThirdParty) httpRequestTokipayPOS(body interface{}, api utils.AP
 	req, _ := http.NewRequest(api.Method, q.endpoint+api.Url+urlExt, requestBody)
 	req.Header.Add("Authorization", q.authorization)
 	req.Header.Add("api_key", "third_party_pay")
-
-	res, err := http.DefaultClient.Do(req)
-
-	response, _ = io.ReadAll(res.Body)
-	if res.StatusCode != 200 {
-		return nil, errors.New(string(response))
-	}
-	return
-}
-
-func (q *tokipayThirdParty) httpRequestTokipayThirdParty(body interface{}, api utils.API, urlExt string) (response []byte, err error) {
-	var requestByte []byte
-	var requestBody *bytes.Reader
-	if body == nil {
-		requestBody = bytes.NewReader(nil)
-	} else {
-		requestByte, _ = json.Marshal(body)
-		requestBody = bytes.NewReader(requestByte)
-	}
-
-	req, _ := http.NewRequest(api.Method, q.endpoint+api.Url+urlExt, requestBody)
-	req.Header.Add("Authorization", q.authorization)
-	req.Header.Add("api_key", "third_party_pay")
-	// req.Header.Add("im_api_key", q.imApiKey)
 
 	res, err := http.DefaultClient.Do(req)
 
